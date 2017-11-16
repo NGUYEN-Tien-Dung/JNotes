778cb9454ef2700e48a7c5fbb01971b52754a530
Merwann Charpentier <merwann.charpentierinsa-cvl.fr>
2017-11-16 16:37:53 +0100

    Maj du fichier Notes.java de Guillaume

--- CS/src/Note.java
+++ CS/src/Note.java
@@ -1,36 +1,153 @@
+package note;
 
-public class Note {
-	
-	private int Id;
-	private String Title;
-	private String Text;
-	private String[] Hashtag;
-	
-	/**
-	 * constructeur de Note
-	 */
-	Note (int Id){
-		
-	}
-	
-	/**
-	 * @return Id de la note
-	 */
-	public int getId() {
-		return Id;
-	}
-	
-	/**
-	 * @return liste des Hashtag compris dans la Note
-	 */
-	public String[] getHashtag() {
-		return Hashtag;
-	}
-	
-	/**
-	 * @return titre de la Note
-	 */
-	public String getTitle() {
-		return Title;
-	}
+// Import permettant entre autres la saisie clavier :
+import java.util.Scanner;
+import java.util.*;   
+
+// Import permettant la gestion des fichiers :
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+// Outils permettant la génération de PDF :
+
+
+// Import permettant de manipuler (lire et écrire des documents XML) :
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Comment;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class Note
+{
+    // Attribus de la classe note (tout est en private, les accesseur et mutateur sont la pour ça) : 
+    private String titre;
+    private String text;
+    private String Hashtag;
+    private int Id; 
+    
+    // Accesseur pour text :
+    public String getText()
+    {
+        return this.text;
+    }
+    
+    // Accesseur pour titre :
+    public String getTitre()
+    {
+        return this.titre;
+    }
+    
+    // Accesseur pour Hastag : 
+    public String getHastag()
+    {
+        return this.Hashtag;
+    }
+    
+    // Accesseur pour Id : 
+    public int getId()
+    {
+        return this.Id;
+    }
+    
+    // Mutateur pour Id : 
+    private void setId(int Identifiant)
+    {
+        this.Id = Identifiant;
+    }
+    
+    // Mutateur pour Text : 
+    private void setTexte(String Text_de_la_note)
+    {
+        this.text = Text_de_la_note;
+    }
+    
+    // Mutateur pour titre :
+    private void setTitre(String Titre_Note)
+    {
+        this.titre = Titre_Note;
+    }
+    
+    // Mutateur pour Hastag :
+    private void setHastag(String Has)
+    {
+        this.Hashtag = Has;
+    }
+   
+    // Remplissage d'une note - UTILISATION FINALE :
+    public void remplir(String TITRE, String TEXT, String HASHTAG, int ID)
+    {
+        this.setHastag(HASHTAG);
+        this.setId(ID);
+        this.setTexte(TEXT);
+        this.setTitre(TITRE);
+    }
+    
+    // Création du dossier dans lequel seront stockée les notes exportées :
+    private void creation_dossier_sauvegarde()
+    {
+        
+    }
+    
+    // Export d'une note au format désiré (XML, HTML, PDF) : 
+    public void export(String export_souhaite)
+    {
+        // Chemain et nom du répertoir où seront stockés les fichiers exportés :
+       
+        if (export_souhaite == "PDF")
+        {
+            
+        }
+        if (export_souhaite == "XML")
+        {
+                    
+        }
+        if (export_souhaite == "HTML")
+        {
+            
+        }
+    }
+    
+    // Remplissage d'une note - POUR LES TESTS  :
+    public void remplir_TESTS()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer le titre de la note :\n");
+        String TITRE = sc.nextLine();
+        System.out.println("Entrer le text de la note :\n");
+        String TEXT = sc.nextLine();
+        System.out.println("Entrer le Hastag de la note :\n");
+        String HASHTAG = sc.nextLine();
+        System.out.println("Entrer l'identifiant de la note :\n");
+        int ID = sc.nextInt();    
+        this.setHastag(HASHTAG);
+        this.setId(ID);
+        this.setTexte(TEXT);
+        this.setTitre(TITRE);
+    }
+    
+    // Affichage d'une note sur console d'application - UNIQUEMENT POUR LES TESTS :
+    public void affiche() 
+    {
+        System.out.println("Le titre de la note est : \n");
+        System.out.println(this.titre);
+        System.out.println("Le text de la note est : \n");
+        System.out.println(this.text);
+        System.out.println("L'identifiant de la note est : \n");
+        System.out.println(this.Id);
+        System.out.println("Le Hastag de la note est : \n");
+        System.out.println(this.Hashtag);
+    }   
 }
+
